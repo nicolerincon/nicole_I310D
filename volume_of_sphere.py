e46bf08678b89b7b52c2a319d7ad4e710990b187
nicolerincon <nicolerinconn@gmail.com>
2025-11-12 21:17:17 -0600

    Add files via upload

---
+++ volume_of_sphere.py
@@ -0,0 +1,8 @@
+import math
+
+def calculate_volume_of_sphere(radius):
+    return (4/3) * math.pi * (radius ** 3)
+
+print("Volume for radius 30:", calculate_volume_of_sphere(30))
+print("Volume for radius 40:", calculate_volume_of_sphere(40))
+
